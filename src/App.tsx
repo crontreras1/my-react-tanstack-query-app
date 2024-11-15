c66264aedabda367ef39a4faf3c73bfcf660d3cc
Cristian Contreras <crontreras1@gmail.com>
2024-11-15 14:19:40 -0500

    class 28. code spliting & error

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { lazy } from "react"
+import { useCoureses } from "./hooks/useCourses"
 
-function App() {
-  const [count, setCount] = useState(0)
+const CourseList = lazy(() => import('./components/CourseList'))
+
+const App: React.FC = () => {
+  const { data: courses, isLoading, error } = useCoureses()
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (error) return <div>Error: { error.message}</div>
+
+  if (!courses) return <div>courses not found</div>
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CourseList courses={} />
     </>
   )
 }
